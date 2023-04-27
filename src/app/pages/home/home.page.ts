5d1d6011d7a5168788d2de88f383343e1ba77693
Jessika Davis <jessikad@united-systems.com>
2023-04-27 11:00:50 -0500

    created the timer functionality.

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { finalize, map, repeat, takeWhile, tap, timer } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
+  public seconds = 10;
+  public isModalOpen = false;
+  
   constructor() {}
+
+  timeRemaining$ = timer(0, 1000).pipe(
+    map(n => (this.seconds - n) * 1000),
+    takeWhile(n => n >= 0),
+    finalize(() => {
+      console.log('Sync finished.')
+    }),
+    repeat()
+  );
+
+  navigateToDBM(): void {
+    //shell.openExternal("https://dbm.thegrindsession.com/");
+    console.log("This takes you to the DBM website.");
+  }
+
+  startSync(): void {
+    this.timeRemaining$ = timer(0, 1000).pipe(
+      map(n => (this.seconds - n) * 1000),
+      takeWhile(n => n >= 0),
+      finalize(() => {
+        console.log('Sync finished.')
+      }),
+      repeat()
+    );
+  }
+
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
 
 }
