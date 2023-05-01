908db4043ed8a097aee081144b7f545bc87419a0
mthompson59 <mthompson59@murraystate.edu>
2023-05-01 14:34:27 -0500

    filter, loader, center-cards

--- src/app/pages/home/components/games/games.component.ts
+++ src/app/pages/home/components/games/games.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { InfiniteScrollCustomEvent, PopoverController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { Game } from 'src/app/interfaces/game.interface';
 import { ApiService } from 'src/app/services/api/api.service';
@@ -14,7 +15,10 @@ import { Logo } from 'src/app/types/logo.type';
 export class GamesComponent implements OnInit {
   public games$?: Observable<Game[]>;
   public logos: Logo[] = []
-
+  public feed: Game[] = [];
+  public gamesList: Game[] = [];
+  private gameCount: number = 15;
+ 
 
   constructor(private common: CommonService, private router: Router, private apiService: ApiService) {}
 
@@ -23,16 +27,39 @@ export class GamesComponent implements OnInit {
     this.common.gameState().subscribe(ready => {
       if (ready) {
         this.games$ = this.common.getGames();
+        this.games$.subscribe(games => {
+          this.gamesList = games;
+          this.feed.push(...this.gamesList.slice(0, this.gameCount-1));
+        });
         this.apiService.getLogos().subscribe(logos => {
           this.logos = logos;
         });
       }
     })
   }
-  
+
+  private async refresh() {
+    this.feed = [];
+    this.gameCount = 15;
+    this.feed.push(...this.gamesList.slice(0, this.gameCount-1));
+  }
+
+  onIonInfinite(ev:any) {
+    setTimeout(() => {
+      if (this.feed.length < this.gamesList.length) {
+        let end = this.gameCount+14;
+        if (this.gameCount > this.gamesList.length)
+          end = this.gamesList.length-1;
+        this.feed.push(...this.gamesList.slice(this.gameCount, end));
+        this.gameCount += 15;
+      }
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 1000);
+  }
 
   public async initCommon() {
     await this.common.initializeService();
+    this.refresh();
   }
 
   navigateToAddGame() {
@@ -43,11 +70,10 @@ export class GamesComponent implements OnInit {
     this.router.navigateByUrl(`/gamecast/${gameId}`);
   }
 
-  public getLogo(teamName: string): string {
+  public getLogos(teamName: string): string {
     const logo = this.logos.find(l => l.team.toLowerCase() === teamName.toLowerCase());
     console.log('teamName:', teamName, 'logo:', logo);
     return logo ? logo.src : '';
   }
 
-
-}
+  }
