6b6acc976f2caf5494d981764e1b92aa90a1e9eb
Jessika Davis <jdavis108@murraystate.edu>
2023-06-30 14:44:57 -0500

    Updated adding players.

--- src/app/pages/add-away-player/add-away-player.component.ts
+++ src/app/pages/add-away-player/add-away-player.component.ts
@@ -1,7 +1,9 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Player } from 'src/app/interfaces/player.interface';
 import { CrudService } from 'src/app/services/crud/crud.service';
-import { SqlService } from 'src/app/services/sql/sql.service';
+import { GamecastComponent } from '../home/components/gamecast/gamecast.component';
+import { SyncState } from 'src/app/interfaces/syncState.enum';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-away-player',
@@ -15,7 +17,37 @@ export class AddAwayPlayerComponent {
 	@Output() dismiss: EventEmitter<void> = new EventEmitter();
 
 	constructor(
-		private route: ActivatedRoute,
 		private crud: CrudService,
-		private sql: SqlService) {}
+		private gameCast: GamecastComponent,
+		private modalController: ModalController
+		) {}
+
+		async addToTeam(team: 'home' | 'away') {
+			let newTeamPlayer: Player = {
+				playerId: 0,
+				firstName: this.newPlayerFirstName || "New",
+				lastName: this.newPlayerLastName || "Player",
+				number: Number(this.newPlayerNumber),
+				position: "",
+				team: team == 'home' ? this.gameCast.currentGame!.homeTeam : this.gameCast.currentGame!.awayTeam,
+				picture: null,
+				isMale: this.gameCast.currentGame!.isMale!,
+				syncState: SyncState.Added,
+				height: null,
+				weight: null,
+				age: null,
+				homeState: null,
+				homeTown: null,
+				socialMediaString: null
+			}
+
+			if (team == 'home') {
+				this.gameCast.homeTeamPlayers!.push(newTeamPlayer);
+			} else {
+				this.gameCast.awayTeamPlayers!.push(newTeamPlayer);
+			}
+
+			await this.crud.save(this.gameCast.db, "Players", newTeamPlayer);
+			return this.modalController.dismiss();
+		}
 }
--- src/app/pages/add-home-player/add-home-player.component.ts
+++ src/app/pages/add-home-player/add-home-player.component.ts
@@ -1,9 +1,9 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Player } from 'src/app/interfaces/player.interface';
 import { SyncState } from 'src/app/interfaces/syncState.enum';
 import { CrudService } from 'src/app/services/crud/crud.service';
-import { SqlService } from 'src/app/services/sql/sql.service';
+import { GamecastComponent } from '../home/components/gamecast/gamecast.component';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-add-home-player',
@@ -17,8 +17,36 @@ export class AddHomePlayerComponent {
 	@Output() dismiss: EventEmitter<void> = new EventEmitter();
 
 	constructor(
-		private route: ActivatedRoute,
 		private crud: CrudService,
-		private sql: SqlService) {}
+		private gameCast: GamecastComponent,
+		private modalController: ModalController) {}
 
+		async addToTeam(team: 'home' | 'away') {
+			let newTeamPlayer: Player = {
+				playerId: 0,
+				firstName: this.newPlayerFirstName,
+				lastName: this.newPlayerLastName,
+				number: Number(this.newPlayerNumber),
+				position: "",
+				team: team == 'home' ? this.gameCast.currentGame!.homeTeam : this.gameCast.currentGame!.awayTeam,
+				picture: null,
+				isMale: this.gameCast.currentGame!.isMale!,
+				syncState: SyncState.Added,
+				height: null,
+				weight: null,
+				age: null,
+				homeState: null,
+				homeTown: null,
+				socialMediaString: null
+			}
+
+			if (team == 'home') {
+				this.gameCast.homeTeamPlayers!.push(newTeamPlayer);
+			} else {
+				this.gameCast.awayTeamPlayers!.push(newTeamPlayer);
+			}
+
+			await this.crud.save(this.gameCast.db, "Players", newTeamPlayer);
+			return this.modalController.dismiss();
+		}
 }
