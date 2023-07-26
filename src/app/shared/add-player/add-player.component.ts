6ed9a1b9101a912905bbd13f379e2bc228768435
Peter Green <peterg@united-systems.com>
2023-07-26 11:28:13 -0500

    TAN-231

--- src/app/shared/add-player/add-player.component.ts
+++ src/app/shared/add-player/add-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { GameCastSettings } from 'src/app/interfaces/gameCastSetting.interface';
 import { Player } from 'src/app/interfaces/player.interface';
 import { SyncState } from 'src/app/interfaces/syncState.enum';
 
@@ -8,6 +9,7 @@ import { SyncState } from 'src/app/interfaces/syncState.enum';
   styleUrls: ['./add-player.component.scss']
 })
 export class AddPlayerComponent {
+	tab:"add"|"hide" = "add";
 	newPlayerNumber!: number;
 	newPlayerFirstName!: string;
 	newPlayerLastName!: string;
@@ -16,7 +18,11 @@ export class AddPlayerComponent {
 	@Input() isMale!:number;
 	@Output() dismiss: EventEmitter<void> = new EventEmitter();
 	@Output() playerAdded: EventEmitter<Player> = new EventEmitter();
+	@Output() playerHidden: EventEmitter<Player> = new EventEmitter();
+	@Output() playerUnhidden: EventEmitter<Player> = new EventEmitter();
 	@Input() color!:string;
+	@Input() players!: Player[];
+	@Input() settings!: GameCastSettings;
 
 	public addToTeam() {
 		let newTeamPlayer: Player = {
@@ -39,5 +45,9 @@ export class AddPlayerComponent {
 
 		this.playerAdded.emit(newTeamPlayer);
 		this.dismiss.emit();
+	}
+
+	isHidden(player:Player) {
+		return this.settings.hiddenPlayers?.split(',').includes(player.playerId);
 	}
 }
