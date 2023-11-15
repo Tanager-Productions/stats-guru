282e94c7f094ca57c7a6ac231ed48855791c22ce
peterg <peterg@tanagerproductions.com>
2023-11-14 23:45:07 -0600

    angular 17

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,7 @@ import { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig  = {
   appId: 'com.tanagerproductions.statsguru',
   appName: 'Stats Guru',
-  webDir: 'dist/stats-guru',
-  bundledWebRuntime: false
+  webDir: 'dist/stats-guru'
 };
 
 export default config;
