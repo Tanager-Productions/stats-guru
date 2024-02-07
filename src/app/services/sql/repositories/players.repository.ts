6508e7b7ce2f863c2f7444433e8f1d519a4fc5c2
Juyeong Ryu <juyeongr@united-systems.com>
2024-02-07 11:38:19 -0600

    done with player repo

--- src/app/services/sql/repositories/players.repository.ts
+++ src/app/services/sql/repositories/players.repository.ts
@@ -38,23 +38,23 @@ export class PlayersRepository implements Repository<sgPlayer, number> {
 		}
 	}
 
-	private mapDtoToDb = (obj: Player): PlayerEntity => {
+	private mapDtoToDb = (obj: sgPlayer): PlayerEntity => {
 		return {
-			id: obj.id,
-			firstName: obj.firstName,
-			lastName: obj.lastName,
-			number: obj.number,
-			position: obj.position,
-			teamId: obj.teamId,
-			picture: obj.picture,
-			isMale: obj.isMale ? 1 : 0,
-			height: obj.height,
-			weight: obj.weight,
-			age: obj.age,
-			homeTown: obj.homeTown,
-			homeState: obj.homeState,
-			socialMediaString: obj.socialMedias != null ? JSON.stringify(obj.socialMedias) : null,
-			infoString: obj.generalInfo != null ? JSON.stringify(obj.generalInfo) : null,
+			id: obj.player.id,
+			firstName: obj.player.firstName,
+			lastName: obj.player.lastName,
+			number: obj.player.number,
+			position: obj.player.position,
+			teamId: obj.player.teamId,
+			picture: obj.player.picture,
+			isMale: obj.player.isMale ? 1 : 0,
+			height: obj.player.height,
+			weight: obj.player.weight,
+			age: obj.player.age,
+			homeTown: obj.player.homeTown,
+			homeState: obj.player.homeState,
+			socialMediaString: obj.player.socialMedias != null ? JSON.stringify(obj.player.socialMedias) : null,
+			infoString: obj.player.generalInfo != null ? JSON.stringify(obj.player.generalInfo) : null,
 			syncState: SyncState.Unchanged
 		}
 	}
@@ -70,7 +70,7 @@ export class PlayersRepository implements Repository<sgPlayer, number> {
 		return players.map(this.mapDbToDto);
 	}
 
-	async add(model: Player): Promise<void> {
+	async add(model: sgPlayer): Promise<void> {
 		const result = await this.db.execute(`
 			INSERT into players (id,
 				firstName,
@@ -105,25 +105,25 @@ export class PlayersRepository implements Repository<sgPlayer, number> {
 				$15,
 				$16)
 		`,
-			[model.id,
-			model.firstName,
-			model.lastName,
-			model.number,
-			model.position,
-			model.teamId,
-			model.picture,
-			model.isMale,
-			model.height,
-			model.weight,
-			model.age,
-			model.homeTown,
-			model.homeState,
-			model.socialMedias,
-			model.generalInfo,
+			[model.player.id,
+			model.player.firstName,
+			model.player.lastName,
+			model.player.number,
+			model.player.position,
+			model.player.teamId,
+			model.player.picture,
+			model.player.isMale,
+			model.player.height,
+			model.player.weight,
+			model.player.age,
+			model.player.homeTown,
+			model.player.homeState,
+			model.player.socialMedias,
+			model.player.generalInfo,
 			SyncState.Added
 			]
 		);
-		model.id = result.lastInsertId;
+		model.player.id = result.lastInsertId;
 	}
 
 	async delete(id: number): Promise<void> {
@@ -134,80 +134,81 @@ export class PlayersRepository implements Repository<sgPlayer, number> {
 		`);
 	}
 
-	async update(model: Player): Promise<void> {
-		await this.db.execute(
-			"UPDATE players SET firstName = $1, lastName = $2, number = $3, position = $4, teamId = $5, picture = $6, isMale = $7, height = $8, weight = $9, age = $10, homeTown = $11, homeState = $12, socialMediaString = $13, infoString = $14, syncState = $15 WHERE id = $16",
-			[model.firstName, model.lastName, model.number, model.position, model.teamId, model.picture, model.isMale, model.height, model.weight, model.age, model.homeTown, model.homeState, model.socialMedias, model.generalInfo, SyncState.Modified, model.id]
+	async update(model: sgPlayer): Promise<void> {
+		await this.db.execute(`
+			UPDATE
+				players
+			SET
+				firstName = $1,
+				lastName = $2,
+				number = $3,
+				position = $4,
+				teamId = $5,
+				picture = $6,
+				isMale = $7,
+				height = $8,
+				weight = $9,
+				age = $10,
+				homeTown = $11,
+				homeState = $12,
+				socialMediaString = $13,
+				infoString = $14,
+				syncState = $15
+			WHERE
+				id = $16`,
+			[model.player.firstName, model.player.lastName, model.player.number, model.player.position, model.player.teamId, model.player.picture, model.player.isMale, model.player.height, model.player.weight, model.player.age, model.player.homeTown, model.player.homeState, model.player.socialMedias, model.player.generalInfo, SyncState.Modified, model.player.id]
 		);
 	}
 
-	async bulkAdd(models: Player[]): Promise<void> {
-		const dbPlayers: PlayerEntity[] = models.map(obj => ({
-			id: obj.id,
-			firstName: obj.firstName,
-			lastName: obj.lastName,
-			number: obj.number,
-			position: obj.position,
-			teamId: obj.teamId,
-			picture: obj.picture,
-			isMale: obj.isMale == true ? 1 : 0,
-			height: obj.height,
-			weight: obj.weight,
-			age: obj.age,
-			homeTown: obj.homeTown,
-			homeState: obj.homeState,
-			socialMediaString: JSON.stringify(obj.socialMedias),
-			infoString: JSON.stringify(obj.generalInfo),
-			syncState: SyncState.Added,
-		}));
-		for (const model of dbPlayers) {
-			await this.db.execute(
-				"INSERT into players (id, firstName, lastName, number, position, teamId, picture, isMale, height, weight, age, homeTown, homeState, socialMediaString, infoString, syncState) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)",
-				[model.id,
-				model.firstName,
-				model.lastName,
-				model.number,
-				model.position,
-				model.teamId,
-				model.picture,
-				model.isMale,
-				model.height,
-				model.weight,
-				model.age,
-				model.homeTown,
-				model.homeState,
-				model.socialMediaString,
-				model.infoString,
-				SyncState.Added
-				]
-			);
-		}
+	async bulkAdd(models: sgPlayer[]): Promise<void> {
+		const dbPlayers: PlayerEntity[] = models.map(this.mapDtoToDb);
+		await this.db.execute(`
+		INSERT
+			into
+			players (id,
+			firstName,
+			lastName,
+			number,
+			position,
+			teamId,
+			picture,
+			isMale,
+			height,
+			weight,
+			age,
+			homeTown,
+			homeState,
+			socialMediaString,
+			infoString,
+			syncState)
+		VALUES ($1,
+		$2,
+		$3,
+		$4,
+		$5,
+		$6,
+		$7,
+		$8,
+		$9,
+		$10,
+		$11,
+		$12,
+		$13,
+		$14,
+		$15,
+		$16)
+		`,
+				[dbPlayers]
+		);
 	}
 
-	async getByGame(game: Game): Promise<Player[]> {
+	async getByGame(game: Game): Promise<sgPlayer[]> {
 		var dbPlayers: PlayerEntity[] = await this.db.select<PlayerEntity[]>(`
 			SELECT 		*
 			FROM 			players
 			WHERE 		teamId = ${game.homeTeamId}
 			OR				teamId = ${game.awayTeamId}
 		`);
-
-		return dbPlayers.map(obj => ({
-			id: obj.id,
-			firstName: obj.firstName,
-			lastName: obj.lastName,
-			number: obj.number,
-			position: obj.position,
-			teamId: obj.teamId,
-			picture: obj.picture,
-			isMale: obj.isMale == 1 ? true : false,
-			height: obj.height,
-			weight: obj.weight,
-			age: obj.age,
-			homeTown: obj.homeTown,
-			homeState: obj.homeState,
-			socialMedias: obj.socialMediaString != null ? JSON.parse(obj.socialMediaString) : null,
-			generalInfo: obj.infoString != null ? JSON.parse(obj.infoString) : null,
-		}));
+		return dbPlayers.map(this.mapDbToDto);
 	}
 }
