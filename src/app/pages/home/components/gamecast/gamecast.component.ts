48814f61d49e85f6778bb5f487af0b4a8a43c7d1
Juyeong Ryu <juyeongr@united-systems.com>
2024-04-02 15:45:00 -0500

    fix for clock

--- src/app/pages/home/components/gamecast/gamecast.component.ts
+++ src/app/pages/home/components/gamecast/gamecast.component.ts
@@ -400,7 +400,7 @@ export class GamecastComponent {
   private startTimer() {
 		const game = { ...this.dataService.game()! };
 		if (game.clock == "00:00") {
-			if (game.period < (game.hasFourQuarters ? 4 : 2)) {
+			if (game.period <= (game.hasFourQuarters ? 4 : 2)) {
 				this.timerDuration = game.minutesPerPeriod! * 60;
 				game.period++;
 				this.dataService.updatePeriod(game.period);
